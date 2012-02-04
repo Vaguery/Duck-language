7423e294014fe6d0ee57a99a3c9172f8a727747a
Bill Tozier <bill@vagueinnovation.com>
2012-02-04 18:11:34 -0500

    cleaning up residue from failed branch

--- spec/interpreter_stack_class_spec.rb
+++ spec/interpreter_stack_class_spec.rb
@@ -2,7 +2,4 @@
 require_relative './spec_helper'
 
 describe "InterpreterStack class" do
-  it "should check to see if it responds to a buffered item after it checks its contents" do
-    
-  end
 end
