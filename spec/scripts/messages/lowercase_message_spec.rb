051964f7d7cdf51ddd26b05de2cf69a0aa185a97
Bill Tozier <bill@vagueinnovation.com>
2012-02-05 13:01:48 -0500

    cleaned up scripts spec folder

---
+++ spec/scripts/messages/lowercase_message_spec.rb
@@ -0,0 +1,15 @@
+#encoding:utf-8
+require_relative '../../spec_helper'
+
+describe "Script" do
+  describe ":lowercase" do
+    it "should be recognized by a Script item" do
+      Script.recognized_messages.should include(:lowercase)
+    end
+
+    it "should produce a new Script with the value downcased" do
+      lc = Script.new("Foo BAR").lowercase
+      lc.value.should == "foo bar"
+    end
+  end
+end
