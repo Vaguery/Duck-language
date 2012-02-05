b63ee9167301b021af1f8e9a43c010bf08a20609
Bill Tozier <bill@vagueinnovation.com>
2012-02-05 13:05:15 -0500

    cleaned up numbers spec folder

---
+++ spec/numbers/messages/<_message_spec.rb
@@ -0,0 +1,12 @@
+#encoding: utf-8
+require_relative '../../spec_helper'
+
+describe "Number" do
+  describe "<" do
+    it "should return the appropriate Bool for two Numbers" do
+      DuckInterpreter.new("2 1 <").run.stack[-1].value.should == false
+      DuckInterpreter.new("1 2 <").run.stack[-1].value.should == true
+      DuckInterpreter.new("1 1 <").run.stack[-1].value.should == false
+    end
+  end
+end
